5fb498a028a0f85a0ea58cb5efc2fd8cf1988b12
Máté Cserháti <mate.cserhati03@gmail.com>
2025-11-16 13:51:13 +0100

    feat: project init

---
+++ src/main/java/com/mtcsrht/bgremovebackend/BgRemoveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mtcsrht.bgremovebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgRemoveBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgRemoveBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mtcsrht/bgremovebackend/BgRemoveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtcsrht.bgremovebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgRemoveBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
